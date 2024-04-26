9a2a873fa12ce1229c3fecc210ecc93fe24c9fd0
Adithya pillai Ramesh <aramesh32@tux-103.cae.wisc.edu>
2024-04-26 15:31:33 -0500

    cpp file changes

--- src/pa3.cpp
+++ src/pa3.cpp
@@ -19,11 +19,17 @@ int main(int argc, char *argv[]){
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    if (argc != ){
-        std::cout << "Usage: ";
+    if (argc != 3 ){
+        std::cout << "Usage: ./pa3 <input_file_name> <output_file_name> ";
+        exit(1);
     }
 
-
+    std::string input_file = argv[1];
+    std::string output_file = argv[2];
+    STClass st;
+    st.read(input_file);
+    st.run();
+    st.output_solution();
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
