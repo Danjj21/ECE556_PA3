1b8df94ddae53ad2976662581fbd397903632c34
Noah James Degroot <ndegroot@tux-145.cae.wisc.edu>
2024-04-26 14:47:04 -0500

    updated cpp file

--- src/pa3.cpp
+++ src/pa3.cpp
@@ -19,14 +19,23 @@ int main(int argc, char *argv[]){
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    if (argc != ){
-        std::cout << "Usage: ";
+    if (argc != 3){
+        std::cout << "Usage: [executable file name] [input file name] [output file name]";
     }
 
+    std::string inputFile = argv[1];
+    std::string outputFile = argv[2];
+
+    STClass st;
+
+    st.readInput(inputFile);
+
+    st.run();
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
+    st.outputSolution(outputFile);
 
     std::cout << "Duration (s): " << duration.count() <<std::endl;
 
